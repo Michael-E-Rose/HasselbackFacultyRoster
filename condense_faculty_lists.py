61d9287a5d65e4117d08f00550b281ba82447204
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2022-07-18 21:35:34 +0200

    More mappings, refactor scripts

--- condense_faculty_lists.py
+++ condense_faculty_lists.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # Author:   Michael E. Rose <michael.ernst.rose@gmail.com>
 """Combines faculty information for faculty members
-with Scopus IDs and PHD.
+with Scopus IDs and PhD.
 """
 
 from collections import Counter
-from glob import glob
 from math import log10
 from os.path import basename, splitext
+from pathlib import Path
 
 import pandas as pd
 from numpy import nan
 
-SOURCE_FOLDER = "./source_files/"
-SCOPUS_FILE = "./mapping_files/persons.csv"
-MAPPING_FILE = "./mapping_files/institutions.csv"
-TARGET_FILE = "./hasselback.csv"
+SOURCE_FOLDER = Path("./source_files/")
+SCOPUS_FILE = Path("./mapping_files/persons.csv")
+MAPPING_FILE = Path("./mapping_files/institutions.csv")
+TARGET_FILE = Path("./hasselback.csv")
 
 
 def complete_year(y):
@@ -41,47 +41,50 @@ def complete_year(y):
 
 def read_hasselback_file(fname, scopus, aff_map, degrees=('PHD',)):
     """Read and compile file, subsetted to relevant degress only."""
-    year = splitext(basename(fname))[0].split("-")[0]
+    year = fname.stem.split("-")[0]
     df = pd.read_csv(fname).dropna(subset=["dep"])
     df = df[df['degree'].isin(degrees)]
     # Aggregate entries
     df['grad_year'] = df['grad_year'].apply(complete_year)
-    df['school'] = df['school'].apply(lambda x: aff_map.get(x, x))
+    df['school'] = df['school'].replace(aff_map)
     id_cols = ['name', 'school', 'grad_year']
     df['ID'] = df[id_cols].fillna("").apply(lambda l: ";".join(l), axis=1)
-    df['ID'] = df['ID'].str.replace(";;", ";").str.replace("nan", "").str.strip(";")
+    df['ID'] = df['ID'].str.replace(";;", ";").str.strip(";")
     # Aggregate departments
+    missing = []
+    for dep in df["dep"].unique():
+        if dep not in aff_map:
+            missing.append(dep)
     df['dep'] = df['dep'].apply(lambda x: aff_map.get(x))
     before = df.shape[0]
-    df = df.dropna(subset=["dep"]).set_index('ID').drop('grad_year', axis=1)
+    df = df.dropna(subset=["dep"]).set_index('ID').drop(columns='grad_year')
     after = df.shape[0]
     print(f">>> {year}: Dropping {before-after} names because of missing "
           "department data")
     # Merge with Scopus
-    df = df.merge(scopus, "left", left_index=True, right_index=True)
+    df = df.join(scopus)
     identified = df[~df['scopus_id'].isnull()].copy()
     identified = identified.set_index("scopus_id").add_prefix(year + "_")
     unidentified = df[df['scopus_id'].isnull()][["dep"]].copy()
-    return identified, unidentified
+    return identified, unidentified, missing
 
 
 def main():
     # Read in
     scopus = pd.read_csv(SCOPUS_FILE, index_col=0)
-    mapping = pd.read_csv(MAPPING_FILE, index_col=0).dropna(subset=['our_name'])
-    mapping = mapping["our_name"].to_dict()
-    files = glob(SOURCE_FOLDER + "*.csv")
-    df, unidentified = read_hasselback_file(files[0], scopus[["scopus_id"]], mapping)
+    inst_map = pd.read_csv(MAPPING_FILE, index_col=0).dropna(subset=['our_name'])
+    inst_map = inst_map["our_name"].to_dict()
+    files = list(SOURCE_FOLDER.glob("*.csv"))
+    df, unidentified, missing = read_hasselback_file(files[0], scopus[["scopus_id"]], inst_map)
     for file in files[1:]:
-        new, new_un = read_hasselback_file(file, scopus[["scopus_id"]], mapping)
-        df = df.merge(new, "outer", left_index=True, right_index=True)
-        unidentified = unidentified.append(new_un)
+        new, new_un, new_miss = read_hasselback_file(file, scopus[["scopus_id"]], inst_map)
+        df = df.join(new, how="outer")
+        missing.extend(new_miss)
+        unidentified = pd.concat([unidentified, new_un])
 
     # Add time-invariant information
-    scopus = scopus.drop_duplicates(subset="scopus_id")
-    scopus = scopus.set_index("scopus_id")
-    df = df.merge(scopus[["scopus_name", 'grad_year']], "left",
-                  left_index=True, right_index=True)
+    scopus = scopus.drop_duplicates(subset="scopus_id").set_index("scopus_id")
+    df = df.join(scopus)
 
     # Maintenance file
     unidentified = unidentified[~unidentified.index.duplicated()]
@@ -106,6 +109,10 @@ def main():
              "N_of_Hasselback_fac": f"{df.shape[0] + unidentified.shape[0]:,}",
              "N_of_Hasselback_dep": len(all_deps)}
     print(stats)
+
+    missing = Counter(missing)
+    print(f">>> Found {len(missing):,} institutions w/o mapping:")
+    print(missing)
 
 
 if __name__ == '__main__':
